ab9b49b4a7c874e267260643d94e3c23fbcac357
Shubhashish Tiwari <shubhashishtiwari08@gmail.com>
2026-02-04 10:31:11 +0530

    deployment requirement setisfied9

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hrmslite-2.onrender.com/",
+  baseURL: "https://hrmslite-2.onrender.com/api/",
 });
 
 export default API;
